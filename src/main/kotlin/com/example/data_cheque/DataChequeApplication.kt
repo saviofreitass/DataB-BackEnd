a91fb0873804f57fce667f3b5355a69cc51d43b6
saviofreitass <saviofreitas04@gmail.com>
2025-04-09 21:53:08 -0300

    commit inicial

---
+++ src/main/kotlin/com/example/data_cheque/DataChequeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.data_cheque
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DataChequeApplication
+
+fun main(args: Array<String>) {
+	runApplication<DataChequeApplication>(*args)
+}
