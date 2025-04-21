4da8780d69fe9639c1f1bbcf734cbcf2bd4e0b71
Kaio <kaiohtf38@gmail.com>
2025-04-21 16:24:07 -0300

    feat: atualizando sql

--- src/main/resources/db/migration/V1__create-usuarios-table.sql
+++ src/main/resources/db/migration/V1__create-usuarios-table.sql
@@ -1,9 +1,7 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE TABLE usuarios (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+CREATE TABLE data_cheque.usuarios (
+    id UUID PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    cpf VARCHAR(11) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
     email VARCHAR(100) UNIQUE NOT NULL,
     senha_hash VARCHAR(255) NOT NULL,
     tipo_usuario VARCHAR(20) NOT NULL CHECK (tipo_usuario IN ('funcionario', 'contador'))
--- src/main/resources/db/migration/V2__create-funcionario-table.sql
+++ src/main/resources/db/migration/V2__create-funcionario-table.sql
@@ -1,14 +1,14 @@
-CREATE TABLE funcionarios (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+CREATE TABLE data_cheque.funcionarios (
+    id UUID PRIMARY KEY,
     usuario_id UUID NOT NULL,
     empregador_id UUID NOT NULL,
     contador_id UUID NOT NULL,
     cargo VARCHAR(100),
     setor VARCHAR(100),
-    data_admissao CURRENT_DATE,
-    salario DOUBLE NOT NULL,
+    data_admissao DATE,
+    salario NUMERIC(10, 2) NOT NULL,
     status BOOLEAN,
-    FOREIGN KEY (usuario_id) REFERENCES usuario(id),
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
     FOREIGN KEY (empregador_id) REFERENCES empregador(id),
     FOREIGN KEY (contador_id) REFERENCES contador(id)
 );
