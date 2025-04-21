e025d53abf4e33dc78c930b5109186aadee19ff0
Kaio <kaiohtf38@gmail.com>
2025-04-21 20:11:15 -0300

    feat: criando migration para criação das tabelas usuarios e funcionarios

--- src/main/resources/db/migration/V1__create-usuarios-table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE data_cheque.usuarios (
-    id UUID PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    cpf VARCHAR(11) NOT NULL UNIQUE,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    senha_hash VARCHAR(255) NOT NULL,
-    tipo_usuario VARCHAR(20) NOT NULL CHECK (tipo_usuario IN ('funcionario', 'contador'))
-);
